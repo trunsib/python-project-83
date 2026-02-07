390e6af3df675e33750291d47dcc1d3fea653391
trun <e.s.trunov@gmail.com>
2026-02-07 17:53:32 +0700

    page_analyzer

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -1,9 +1,11 @@
 import os
+
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 def get_connection():
     database_url = os.getenv('DATABASE_URL')
