abe26f53083f3b678562a52bcb3bb9a66b00de4b
trun <e.s.trunov@gmail.com>
2026-05-12 12:36:43 +0700

    page_analyzer

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -10,11 +10,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 from page_analyzer.db import get_connection
 
-def truncate(text, length=255):
+def truncate(text, limit=255):
     if not text:
         return text
     text = text.strip()
-    return text if len(text) <= length else text[:length - 3] + "..."
+    return text if len(text) <= limit else text[:limit - 3] + "..."
 
 load_dotenv()
 
